3a09d4f4fd9f9ddaf95578bb4b0c3afea94b4a69
SofianeAbk <sofiane.aboulkabila@gmail.com>
2024-03-04 12:09:53 +0100

    update

--- index.js
+++ index.js
@@ -5,47 +5,33 @@ import axios from 'axios';
 const dataToWrite = 'Hello, world!';
 const PORT = 8000;
 
-const server = http.createServer(async (req, res) => {
-    try {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>Hello, world!</h1>');
-    } catch (error) {
-        console.log('Error sending response:', error);
-    }
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello, world!</h1>');
 });
 
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
 
-async function fetchDataAndWriteToFile() {
-    try {
-        const response = await axios.get('http://google.com/');
-        await fs.promises.writeFile('google.html', response.data);
-        console.log('The HTML file has been saved!');
-    } catch (error) {
-        console.log('Error fetching data and writing to file:', error);
-    }
-}
+axios.get(`http://google.com/`)
+    .then(response => {
+        fs.writeFile('google.html', response.data, (err) => {
+            if (err) throw err;
+            console.log('The HTML file has been saved!');
+        });
+    })
+    .catch(error => {
+        console.log('Error fetching Localhost:', error);
+    });
 
-async function writeDataToFile() {
-    try {
-        await fs.promises.writeFile('message.txt', dataToWrite);
-        console.log('The file has been saved!');
-    } catch (error) {
-        console.log('Error writing to file:', error);
-    }
-}
 
-async function readDataFromFile() {
-    try {
-        const data = await fs.promises.readFile('message.txt', 'utf8');
-        console.log(data);
-    } catch (error) {
-        console.log('Error reading from file:', error);
-    }
-}
+fs.writeFile('message.txt', dataToWrite, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+});
 
-fetchDataAndWriteToFile();
-writeDataToFile();
-readDataFromFile();
+fs.readFile('message.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
