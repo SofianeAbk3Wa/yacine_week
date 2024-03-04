58195a82d157a94b2868fd5eee0ce88e3afc5863
SofianeAbk <sofiane.aboulkabila@gmail.com>
2024-03-04 15:33:29 +0100

    update

--- languages/languages.js
+++ languages/languages.js
@@ -1,48 +1,51 @@
-import express from 'express'
+import express from 'express';
 
-const languagesRouter = express.Router()
+const languagesRouter = express.Router();
 
 const languages = [
-    {id: 1, name: 'Anglais', famille: 'Germanique'},
-    {id: 2, name: 'Espagnol', famille: 'Romane'},
-    {id: 3, name: 'Français', famille: 'Romane'},
-    {id: 4, name: 'Allemand', famille: 'Germanique'}
-]
+    { id: 1, name: 'Anglais', famille: 'Germanique' },
+    { id: 2, name: 'Espagnol', famille: 'Romane' },
+    { id: 3, name: 'Français', famille: 'Romane' },
+    { id: 4, name: 'Allemand', famille: 'Germanique' }
+];
 
-languagesRouter.get('/langues', (req, res) => {
-    res.json(languages)
-})
+// GET: Récupérer toutes les langues
+// GET: Récupérer une langue par ID
+// POST: Créer une nouvelle langue
+// PUT: Mettre à jour une langue par ID
+// DELETE: Supprimer une langue par ID
 
-languagesRouter.get('/langues/:id', (req, res) => {
-    let {id} = req.params
-    const langue = languages.find(langue => langue.id === parseInt(id))
-    res.json(langue)
-})
+languagesRouter
+    .get('/langues', (req, res) => {
+        res.json(languages);
+    })
+    .get('/langues/:id', (req, res) => {
+        let { id } = req.params;
+        const langue = languages.find(langue => langue.id === parseInt(id));
+        res.json(langue);
+    })
+    .post('/langues', (req, res) => {
+        const langue = {
+            id: languages.length + 1,
+            name: req.body.name,
+            famille: req.body.famille
+        };
+        languages.push(langue);
+        res.json(langue);
+    })
+    .put('/langues/:id', (req, res) => {
+        let { id } = req.params;
+        const langue = languages.find(langue => langue.id === parseInt(id));
+        langue.name = req.body.name;
+        langue.famille = req.body.famille;
+        res.json(langue);
+    })
+    .delete('/langues/:id', (req, res) => {
+        let { id } = req.params;
+        const langue = languages.find(langue => langue.id === parseInt(id));
+        const langueId = languages.indexOf(langue);
+        languages.splice(langueId, 1);
+        res.json('Langue supprimée avec succès');
+    });
 
-languagesRouter.post('/langues', (req, res) => {
-    const langue = {
-        id: languages.length + 1,
-        name: req.body.name,
-        famille: req.body.famille
-    }
-    languages.push(langue)
-    res.json(langue)
-})
-
-languagesRouter.put('/langues/:id', (req,res) => {
-    let {id} = req.params
-    const langue = languages.find(langue => langue.id === parseInt(id))
-    langue.name = req.body.name
-    langue.famille = req.body.famille
-    res.json(langue)
-})
-
-languagesRouter.delete('/langues/:id', (req,res) => {
-    let {id} = req.params
-    const langue = languages.find(langue => langue.id === parseInt(id))
-    const langueId = languages.indexOf(langue)
-    languages.splice(langueId, 1)
-    res.json('Langue supprimée avec succès')
-})
-
-export default languagesRouter
+export default languagesRouter;
