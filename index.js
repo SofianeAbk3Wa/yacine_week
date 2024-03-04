de81a624036bcc0933a360c190b54419884271df
SofianeAbk <sofiane.aboulkabila@gmail.com>
2024-03-04 16:52:43 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import languageRouter from './languages/languages.js';
-
 import path from 'node:path';
 import process from 'node:process';
 
@@ -10,6 +9,14 @@ const port = 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(process.cwd(), 'views')));
+
+app.use((req, res, next) => {
+    if (req.originalUrl.endsWith('.css')) {
+        res.type('text/css');
+    }
+    next();
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(process.cwd(), 'views'));
