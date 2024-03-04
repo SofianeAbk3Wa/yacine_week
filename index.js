5c5333672555038bc7727d3cc51af0de2ea1a119
SofianeAbk <sofiane.aboulkabila@gmail.com>
2024-03-04 11:58:38 +0100

    Modification index

--- index.js
+++ index.js
@@ -30,3 +30,8 @@ fs.writeFile('message.txt', dataToWrite, (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
 });
+
+fs.readFile('message.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
