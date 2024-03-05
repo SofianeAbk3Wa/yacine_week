315faf83a0c4e99d24ab65759393998a89fa92d9
SofianeAbk <sofiane.aboulkabila@gmail.com>
2024-03-05 17:21:08 +0100

    update

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -13,6 +13,17 @@ userRouter.get('/users', async (req, res) => {
     }
 });
 
+userRouter.get('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await User.findById(id);
+        res.json(user);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+
 userRouter.post('/users', async (req, res) => {
     try {
         const newUser = await User.create(req.body);
