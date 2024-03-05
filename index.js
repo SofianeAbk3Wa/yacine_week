70c4ed41184904304835bca465f3f833143c8a81
SofianeAbk <sofiane.aboulkabila@gmail.com>
2024-03-05 16:07:03 +0100

    update

--- index.js
+++ index.js
@@ -10,14 +10,20 @@ const app = express();
 const port = process.env.PORT || 3000;
 const mongoDB ='mongodb+srv://admin:lI7VTtMkrTaDPejk@yacine-week.rwnonkm.mongodb.net/?retryWrites=true&w=majority&appName=yacine-week';
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+// Middleware
+app.use(
+    bodyParser.urlencoded({ extended: true }),
+    express.urlencoded({ extended: true }),
+    express.json()
+);
 
-app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
-
+// MongoDB connection
 mongoose.connect(mongoDB)
-  .then(() => console.log('MongoDB connected successfully!'))
-  .catch(err => console.error('MongoDB connection error:', err));
+    .then(() => console.log('MongoDB connected successfully!'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
+// Routes
 app.use('/', routes);
+
+// Start server
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
