1cc54ef029fb55aa98a4bd56b00b825da167dd9a
SofianeAbk <sofiane.aboulkabila@gmail.com>
2024-03-05 15:55:25 +0100

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import routes from './routes/index.js';
 
 dotenv.config();
 
@@ -10,10 +11,10 @@ const port = process.env.PORT || 3000;
 const mongoDB ='mongodb+srv://admin:lI7VTtMkrTaDPejk@yacine-week.rwnonkm.mongodb.net/?retryWrites=true&w=majority&appName=yacine-week';
 
 app.use(bodyParser.urlencoded({ extended: true })).use(express.json()).use(express.urlencoded({ extended: true }));
-
-mongoose.connect(mongoDB);
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
+
+mongoose.connect(mongoDB)
+  .then(() => console.log('MongoDB connected successfully!'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+  app.use('/', routes);
