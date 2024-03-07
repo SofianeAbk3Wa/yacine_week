7c6d497db933f748db9bcb591517a60cd9de7808
SofianeAbk <sofiane.aboulkabila@gmail.com>
2024-03-07 14:21:28 +0100

    update

--- api/app.js
+++ api/app.js
@@ -3,15 +3,15 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import process from 'node:process';
+
 import userRouter from './routes/userRouter.js';
 import postRouter from './routes/postRouter.js';
 import authRouter from './routes/authRouter.js';
 
-dotenv.config();
-
 //? --------------------------------------------------------------------------------
 //? INITIALISATION
 //? --------------------------------------------------------------------------------
+dotenv.config();
 const app = express();
 const { PORT, MONGO_URI } = process.env;
 const router = express.Router();
@@ -24,20 +24,24 @@ app.use(json());
 app.use(cors());
 
 //? --------------------------------------------------------------------------------
+//?  ROUTES
+//? --------------------------------------------------------------------------------
+
+
+app.use('/', userRouter, postRouter, authRouter);
+
+router.get('/', (req, res) => {
+    res.send('INDEX ROUTE');
+});
+
+
+//? --------------------------------------------------------------------------------
 //? CONNECTION TO MONGODB
 //? --------------------------------------------------------------------------------
 mongoose.connect(MONGO_URI)
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(err => console.error('Erreur de connexion à MongoDB:', err));
 
-//? --------------------------------------------------------------------------------
-//?  ROUTES
-//? --------------------------------------------------------------------------------
-router.get('/', (req, res) => {
-    res.send('INDEX ROUTE');
-});
-
-app.use('/', userRouter, postRouter, authRouter);
 
 //? --------------------------------------------------------------------------------
 //? LAUNCH SERVER
